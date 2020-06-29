1a95609f60c66b56403b89b6c68b74ba16e80a1f
MCLEANS <jackagwaya@gmail.com>
2020-06-29 22:32:48 +0300

    test changes

--- IMPLEMENTATION/src/main.cpp
+++ IMPLEMENTATION/src/main.cpp
@@ -12,6 +12,7 @@
 #include "stm32f4xx.h"
 #include "clockconfig.h"
 
+
 custom_libraries::clock_config system_clock;
 
 int main(void)
@@ -22,37 +23,42 @@ int main(void)
 	 * APB1 at 42MHz
 	 * APB2 at 84MHz
 	 */
+
 	system_clock.initialize();
 
 	//Enable USART RCC (USART1)
-	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+	RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
 
 	//Set USART1 pins to Alternate Function Mode
-	GPIOA->MODER |= GPIO_MODER_MODER9_1;
-	GPIOA->MODER |= GPIO_MODER_MODER10_1;
+	GPIOB->MODER |= GPIO_MODER_MODER10_1;
+	GPIOB->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR10;
+	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR10_0;
+
 
 	//Enable the specific alternate function
-	GPIOA->AFR[1] |= (1<<4);
-	GPIOA->AFR[1] |= (1<<5);
-	GPIOA->AFR[1] |= (1<<6);
+	GPIOB->AFR[1] |= (1<<8);
+	GPIOB->AFR[1] |= (1<<9);
+	GPIOB->AFR[1] |= (1<<10);
 
-	GPIOA->AFR[1] |= (1<<8);
-	GPIOA->AFR[1] |= (1<<9);
-	GPIOA->AFR[1] |= (1<<10);
+	//GPIOA->AFR[1] |= (1<<8);
+	//GPIOA->AFR[1] |= (1<<9);
+	//GPIOA->AFR[1] |= (1<<10);
 
 	//Set the UART baudrate(9600)
-	USART1->BRR = 0x222E;
+	USART3->BRR = 0x1117;
 
 	//Enable RX, TX and UART
-	USART1->CR1 |= USART_CR1_TE;
-	USART1->CR1 |= USART_CR1_RE;
-	USART1->CR1 |= USART_CR1_UE;
-
+	USART3->CR1 |= USART_CR1_TE;
+	USART3->CR1 |= USART_CR1_RE;
+	USART3->CR1 |= USART_CR1_UE;
 
 	while(1){
 
-		USART1->DR = 'A';
-		while(!(USART1->SR & USART_SR_TXE));
+		USART3->DR = 0x4D;
+		while(!(USART3->SR & USART_SR_TXE)){}
+
+
 
 	}
 }
