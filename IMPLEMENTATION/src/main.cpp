f1bdf3e0d1594d423a42eaa851e9bcfddaf8ce4c
MCLEANS <jackagwaya@gmail.com>
2020-07-16 22:06:09 +0300

    fixed UART transmission baudrate

--- IMPLEMENTATION/src/main.cpp
+++ IMPLEMENTATION/src/main.cpp
@@ -15,50 +15,52 @@
 
 custom_libraries::clock_config system_clock;
 
+void send_char(char byte){
+	while(!(USART3->SR & USART_SR_TXE)){}
+	USART3->DR = byte;
+}
+
+void send_string(char *byte){
+	for(;*byte;byte++){
+		send_char(*byte);
+	}
+}
+
 int main(void)
 {
-	/*
-	 * Initialize system clock
-	 * AHB at 168MHz
-	 * APB1 at 42MHz
-	 * APB2 at 84MHz
-	 */
 
 	system_clock.initialize();
-
-	//Enable USART RCC (USART1)
+	//Enable USART RCC (USART3)
 	RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
 
-	//Set USART1 pins to Alternate Function Mode
+	//Set USART3 pins to Alternate Function Mode
 	GPIOB->MODER |= GPIO_MODER_MODER10_1;
-	GPIOB->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR10;
+	GPIOB->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR10_0;
 	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR10_0;
-
 
 	//Enable the specific alternate function
 	GPIOB->AFR[1] |= (1<<8);
 	GPIOB->AFR[1] |= (1<<9);
 	GPIOB->AFR[1] |= (1<<10);
 
-	//GPIOA->AFR[1] |= (1<<8);
-	//GPIOA->AFR[1] |= (1<<9);
-	//GPIOA->AFR[1] |= (1<<10);
-
-	//Set the UART baudrate(9600)
-	USART3->BRR = 0x1117;
+	uint16_t baud = (42000000/(9600));
 
 	//Enable RX, TX and UART
-	USART3->CR1 |= USART_CR1_TE;
+    USART3->CR1 |= USART_CR1_TE;
 	USART3->CR1 |= USART_CR1_RE;
 	USART3->CR1 |= USART_CR1_UE;
 
+	//Set the UART baudrate(9600)
+	USART3->BRR = ( ( ( baud / 16 ) << 4) |
+            ( ( (baud % 16 )<< 0) ) );
+
+
+	char name[] = "JACK MCLEANS";
+	send_string(name);
+
+
 	while(1){
-
-		USART3->DR = 0x4D;
-		while(!(USART3->SR & USART_SR_TXE)){}
-
-
 
 	}
 }
