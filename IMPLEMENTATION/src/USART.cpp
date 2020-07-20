be52e5a8f0030738bf821a289acc364e74e8806c
MCLEANS <jackagwaya@gmail.com>
2020-07-20 12:09:07 +0300

    USART library files

---
+++ IMPLEMENTATION/src/USART.cpp
@@ -0,0 +1,21 @@
+/*
+ * USART.cpp
+ *
+ *  Created on: Jul 20, 2020
+ *      Author: MCLEANS
+ */
+
+#include "USART.h"
+
+namespace custom_libraries {
+
+USART::USART() {
+	// TODO Auto-generated constructor stub
+
+}
+
+USART::~USART() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace custom_libraries */
---
+++ IMPLEMENTATION/src/USART.h
@@ -0,0 +1,22 @@
+/*
+ * USART.h
+ *
+ *  Created on: Jul 20, 2020
+ *      Author: MCLEANS
+ */
+
+#ifndef USART_H_
+#define USART_H_
+
+namespace custom_libraries {
+
+class USART {
+
+public:
+	USART();
+	virtual ~USART();
+};
+
+} /* namespace custom_libraries */
+
+#endif /* USART_H_ */
